d91244ecfc835b952167a837b8ac9d79d691a56e
Ricky JW <rickywid@hotmail.com>
2016-05-22 18:00:55 -0400

    asadf

--- config/database.php
+++ config/database.php
@@ -58,8 +58,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-  /* 
-
+ /*  
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
@@ -73,7 +72,8 @@ return [
             'strict' => false,
             'engine' => null,
         ],
-*/ 
+*/  
+
         'mysql' => [
             'driver' => 'mysql',
             'port' => env('DB_PORT', '3306'),
@@ -88,7 +88,7 @@ return [
             'engine' => null,
         ],
   
- 
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
